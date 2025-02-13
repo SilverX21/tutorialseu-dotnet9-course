a856b011b4658144833efca0bb60fd137560ad87
SilverX21 <nuno21araujo95@gmail.com>
2025-02-13 01:23:28 +0000

    Add authorization and view model for job postings
    Updated JobPostingsController to include new using directives and authorization attributes. Modified Index method to allow anonymous access and updated Create method to use JobPostingViewModel with role-based authorization. Added a new Delete method for job posting deletions.
    
    Updated Create.cshtml to use JobPostingViewModel with input fields and validation messages. Added a section for validation scripts.
    
    Updated Index.cshtml to display a list of job postings with details.
    
    Added JobPostingViewModel.cs to define a view model for job postings with required fields.

--- Identity/DevSpot/Controllers/JobPostingsController.cs
+++ Identity/DevSpot/Controllers/JobPostingsController.cs
@@ -1,11 +1,14 @@
 ﻿using DevSpot.Models;
 using DevSpot.Repositories;
+using DevSpot.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevSpot.Controllers;
 
-//[Authorize(Roles.JobSeeker)]
+//authorize attribute is used to restrict access to the controller to only authenticated users
+[Authorize]
 public class JobPostingsController : Controller
 {
     private readonly IRepository<JobPosting> _repository;
@@ -19,6 +22,8 @@ public class JobPostingsController : Controller
         _userManager = userManager;
     }
 
+    //allow anonymous users to access the index page
+    [AllowAnonymous]
     public async Task<IActionResult> Index()
     {
         var jobPostings = await _repository.GetAllAsync();
@@ -26,14 +31,44 @@ public class JobPostingsController : Controller
         return View(jobPostings);
     }
 
+    //restrict access to the create page to only users with the roles Admin and Employer
+    [Authorize(Roles = "Admin,Employer")]
     public IActionResult Create()
     {
         return View();
     }
 
     [HttpPost]
-    public IActionResult Create(JobPosting jobPosting)
+    [Authorize(Roles = "Admin,Employer")]
+    public async Task<IActionResult> Create(JobPostingViewModel jobPostingVm)
     {
-        return View();
+        //we can mofdify the validations for the model and remove some of the validations like this: (this is not a good practice)
+        //ModelState.Remove("UserId");
+        //ModelState.Remove("User");
+
+        if (ModelState.IsValid)
+        {
+            //we can get the current user by using the object "User", that has the current user data. This will only work if there is a user logged in
+            var jobPosting = new JobPosting
+            {
+                Title = jobPostingVm.Title,
+                Description = jobPostingVm.Description,
+                Company = jobPostingVm.Company,
+                Location = jobPostingVm.Location,
+                UserId = _userManager.GetUserId(User)
+            };
+
+            await _repository.AddAsync(jobPosting);
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(jobPostingVm);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _repository.DeleteAsync(id);
+        return RedirectToAction(nameof(Index));
     }
 }
